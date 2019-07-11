eb2c20a86f02723638636e3169ef74f682b15613
Iryna <iriskag@ukr.net>
2019-07-11 18:41:52 +0300

    Updates

---
+++ precache-manifest.acafbe204df58f78f1883fa4ce2e9c07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e92cbd66d8b8b064840ba8c5795243",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "ddff313dc7c515ff2575",
+    "url": "/react_people-table/static/css/main.dae85f77.chunk.css"
+  },
+  {
+    "revision": "7dabeb69257e7a6c32d4",
+    "url": "/react_people-table/static/js/2.21a54373.chunk.js"
+  },
+  {
+    "revision": "ddff313dc7c515ff2575",
+    "url": "/react_people-table/static/js/main.8c8518bb.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_people-table/precache-manifest.3d8c6b1e7697442ca25d7e26bf334fd1.js"
+  "/react_people-table/precache-manifest.acafbe204df58f78f1883fa4ce2e9c07.js"
 );
 
 self.addEventListener('message', (event) => {
